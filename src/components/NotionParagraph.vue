fbc6a5813d3036e797105aa57f7f046427a8282d
Eduard Aksamitov <e@edaks.co>
2022-02-10 21:06:22 +0300

    chore: simplify `notion-paragraph`

--- src/components/NotionParagraph.vue
+++ src/components/NotionParagraph.vue
@@ -1,26 +1,16 @@
-<script lang="ts">
-import { defineComponent, h } from 'vue'
-import type { PropType, VNodeChild } from 'vue'
+<template lang="pug">
+p
+	template(v-for="item in paragraph")
+		template(v-if="item.type === 'text'") {{ item.content }}
+		template(v-else)
+			a-link(:href="item.href") {{ item.content }}
+</template>
 
+<script lang="ts" setup>
+import ALink from './ALink.vue'
 import type { ArticleBlockParagraphContent } from '../../build/types'
 
-export default defineComponent({
-	props: {
-		paragraph: {
-			type: Array as PropType<ArticleBlockParagraphContent[]>,
-			required: true
-		}
-	},
-	setup (props) {
-		return (): VNodeChild => h('p', props.paragraph.map(item => {
-			return item.type === 'text'
-				? item.content
-				: h('a', {
-					href: item.href,
-					target: '_blank',
-					rel: 'noopener'
-				}, item.content)
-		}))
-	}
-})
+defineProps<{
+	paragraph: ArticleBlockParagraphContent[]
+}>()
 </script>
