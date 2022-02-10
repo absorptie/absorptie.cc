bca8ee8fbd5038fe93e7f2d11b9770516f365447
Eduard Aksamitov <e@edaks.co>
2022-02-10 20:31:42 +0300

    feat: `a-description-list`

---
+++ src/components/ADescriptionList.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+dl.a-dl
+	dt
+		slot(name="dt")
+	dd
+		slot(name="dd")
+</template>
+
+<style lang="stylus" scoped>
+.a-dl
+	display: grid
+	grid-column: auto / span 2
+	gap: 10px
+
+	@media (max-width: 830px)
+		max-width: 180px
+		padding-bottom: 30px
+</style>
