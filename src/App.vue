69c2dcdaf0bd4db3628b1f6fd4ca7617d231c484
Eduard Aksamitov <e@edaks.co>
2022-02-16 22:03:53 +0300

    feat: 'app'

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+the-header
+main.main
+	component(:is="component")
+the-footer
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+import { router } from './router'
+import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
+
+const component = computed(() => router.component.value)
+</script>
+
+<style lang="stylus" scoped>
+.main
+	display: flex
+	flex-direction: column
+	gap: 301px
+</style>
